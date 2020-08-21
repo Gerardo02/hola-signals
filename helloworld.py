a0433bff6618173bb09bedc83ad40b3f57f3f9f6
Gerardo02 <gerardo.jerez@hotmail.com>
2020-08-21 01:35:19 -0700

    cosas basicas

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,23 @@
-print("Hola mundo")
+import math
+
+print("Hola")
+
+#convertidor a radianes
+
+def convertidor(grados):
+    radianes = (grados * math.pi)/180
+
+    return radianes
+
+print("El resultado en radianes es: " + str(convertidor(45)))
+seno = math.sin(convertidor(60))
+print("Y el seno es: " + str(seno))
+
+
+#concatencion usando una funcion
+
+def concatenar(str1, str2):
+    return str1 + str2
+
+print(concatenar("Hola me llamo ", "Gerardo"))
+
